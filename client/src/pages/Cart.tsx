4b9f432a38583a20dd5f54bd2a61b3396c06b4d6
iambenjo <benjoquilario@gmail.com>
2022-10-22 15:25:37 +0000

    add total item in cart and overall price

--- client/src/pages/Cart.tsx
+++ client/src/pages/Cart.tsx
@@ -1,10 +1,10 @@
-import { Link } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from '../hooks';
-import { RootState } from '../store/store';
-import { cartAddItem, removeItem } from '../store/cart/slice';
-import { IProduct } from '../services/products';
+import { Link } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../hooks";
+import { RootState } from "../store/store";
+import { cartAddItem, removeItem } from "../store/cart/slice";
+import { IProduct } from "../services/products";
 
-const Cart = () => {
+const Cart = (): JSX.Element => {
   const dispatch = useAppDispatch();
   const { cartList, totalPrice, totalItems } = useAppSelector(
     (state: RootState) => state.cart
@@ -19,49 +19,53 @@ const Cart = () => {
           <Link to="/">Go Shopping</Link>
         </div>
       ) : (
-        <div>
-          {cartList.map((item: IProduct) => (
-            <div key={item._id}>
-              <p>{item.name}</p>
-              <p>{item.quantity}</p>
-              {item.price}
-              <button
-                onClick={() =>
-                  dispatch(
-                    cartAddItem({ ...item, quantity: item.quantity - 1 })
-                  )
-                }
-                disabled={item.quantity === 0}
-              >
-                -
-              </button>
-              <button onClick={() => dispatch(removeItem(item))}>Delete</button>
+        <>
+          <div>
+            {cartList.map((item: IProduct) => (
+              <div key={item._id}>
+                <p>{item.name}</p>
+                <p>{item.quantity}</p>
+                {item.price}
+                <button
+                  onClick={() =>
+                    dispatch(
+                      cartAddItem({ ...item, quantity: item.quantity - 1 })
+                    )
+                  }
+                  disabled={item.quantity === 0}
+                >
+                  -
+                </button>
+                <button onClick={() => dispatch(removeItem(item))}>
+                  Delete
+                </button>
 
-              <button
-                onClick={() =>
-                  dispatch(
-                    cartAddItem({ ...item, quantity: item.quantity + 1 })
-                  )
-                }
-                disabled={item.countInStock <= item.quantity}
-              >
-                +
-              </button>
+                <button
+                  onClick={() =>
+                    dispatch(
+                      cartAddItem({ ...item, quantity: item.quantity + 1 })
+                    )
+                  }
+                  disabled={item.countInStock <= item.quantity}
+                >
+                  +
+                </button>
+              </div>
+            ))}
+          </div>
+          <div className="col-1">
+            <div className="card card-body">
+              <ul>
+                <li>
+                  <h2>
+                    Subtotal ({totalItems}):$({totalPrice})
+                  </h2>
+                </li>
+              </ul>
             </div>
-          ))}
-        </div>
+          </div>
+        </>
       )}
-      <div className="col-1">
-        <div className="card card-body">
-          <ul>
-            <li>
-              <h2>
-                Subtotal ({totalItems}):$({totalPrice})
-              </h2>
-            </li>
-          </ul>
-        </div>
-      </div>
     </div>
   );
 };
