505729b858a50660de721c7379aa3d1dffcf9e20
iambenjo <benjoquilario@gmail.com>
2022-10-23 02:39:01 +0000

    add database and middleware routes

--- index.ts
+++ index.ts
@@ -7,10 +7,11 @@ import data from "./data";
 
 import { notFound, errorHandler } from "./middleware/errorMiddleware";
 import seedRouter from "./routes/seed";
+import productsRouter from "./routes/product";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI as string;
+const MONGO_URI: string = process.env.MONGO_URI as string;
 
 const app = express();
 app.use(express.json());
@@ -18,20 +19,15 @@ app.use(cookieParser());
 app.use(cors());
 
 // Routes
-
 app.use("/api/seed", seedRouter);
+app.use("/products", productsRouter);
 
 // app.get("/api/products/:id", (req: Request, res: Response) => {
 //   const product = data.products.find((x) => x._id === req.params.id);
 
-//   console.log(product);
 //   if (product) res.send(product);
 //   else res.status(404).send({ message: "Product not found" });
 // });
-
-app.get("/api/products", (req: Request, res: Response) => {
-  res.send(data.products);
-});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello");
