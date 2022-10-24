08cdd619a25c11faec12347253c82a7ffc5e8051
iambenjo <benjoquilario@gmail.com>
2022-10-24 12:51:45 +0000

    add loginusers post route and add expressAsyncHandler

---
+++ controllers/seed.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import data from "../data";
+import Product from "../models/product";
+import User from "../models/user";
+
+/**
+ * Insert the fake data to the database
+ * @routes GET /api/seed
+ * @access Private
+ */
+export const getFakeData = (req: Request, res: Response) => {
+  Product.insertMany(data.products);
+  User.insertMany(data.users);
+};
