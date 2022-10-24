8f7788c86f4a8598302f013544bb6176877ae87c
iambenjo <benjoquilario@gmail.com>
2022-10-24 12:49:56 +0000

    add users controllers and routes

---
+++ models/user.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+      require: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
