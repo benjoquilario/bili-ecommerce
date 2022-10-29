a1f241af6fc60842785884be34b02b75d60631b7
iambenjo <benjoquilario@gmail.com>
2022-10-29 09:09:42 +0000

    add auth user authorization

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,19 @@
+import { Request as Req, Response as Res, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
-export const isAuth = (req, res, next) => {
+interface IUser {
+  user?: {
+    _id: string;
+    name: string;
+    email: string;
+    isAdmin?: boolean;
+  };
+}
+
+type Request = Req & IUser;
+type Response = Res & IUser;
+
+export const isAuth = (req: Request, res: Response, next: NextFunction) => {
   const authorization = req.headers.authorization;
   if (authorization) {
     const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
